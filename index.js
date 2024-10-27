776d6b4f33ad2e89abe2a680e4ae9f50e3fed848
MajidKh230 <khanmajid230@gmail.com>
2024-10-28 00:27:08 +0500

    connected to mongodbatlas

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello")
+require("dotenv").config()
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from node server API updated");
+});
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log('connection established')
+})
+.catch(()=>{
+    console.log("connection failed")
+})
